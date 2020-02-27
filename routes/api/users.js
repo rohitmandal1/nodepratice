9ebd49b3eadf9deb2df3e6ff7bdbb41d6640fa02
Rohit mandal <ronitrocksrohit@gmail.com>
2020-02-27 16:02:22 +0530

    add aws cognito

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,41 +1,51 @@
 const express = require('express');
 const router = express.Router();
+const AmazonCognitoIdentity = require('amazon-cognito-identity-js');
 const bcrypt = require('bcryptjs');
 const jwt	 = require('jsonwebtoken');
-const keys = require('../../config/keys');
-const passport = require('passport');
+//const keys = require('../../config/keys');
+//const passport = require('passport');
+
+
+const config = require('./config.json');
+
+// Aws cognito userpool
+const Pooldata = {
+	UserPoolId: config.cognito.userPoolId,
+	ClientId: config.cognito.clientId
+};
+
+const userPool = new AmazonCognitoIdentity.CognitoUserPool(Pooldata);
 // User Model
 const User = require('../../models/User');
 // Register validation 
-const { validateRegisterInput } = require('../../validation/user.validation');
+//const { validateRegisterInput } = require('../../validation/user.validation');
 
 // Login validation 
 const { validateLoginInput } = require('../../validation/user.validation');
 
 //Register user detail
-router.post('/register', validateRegisterInput,(req,res) => {
-	User.findOne({email: req.body.email})
-		.then(user => {
-			if(user){
-			  return res.status(400).json({ email: 'Email allready Exists'});
-			}{
-				newUser = new User({
-					name: req.body.name,
-					email: req.body.email,
-					password: req.body.password
-				});
+router.post('/register', (req, res) => {
+	
+		name= req.body.name;
+		email= req.body.email;
+		password= req.body.password;
+	
 
-				bcrypt.genSalt(10, (err, salt) => {
-					bcrypt.hash(newUser.password, salt, (err, hash)=>{
-						if (err) throw err;
-						newUser.password = hash;
-						newUser.save()
-							.then(user => res.json(user))
-							.catch(err => console.log(err));
-					})
-				})
-			}
-		})
+	const emailData = {
+		Name: 'email',
+		Value: email
+
+	};
+	
+	const emailAttribute = new AmazonCognitoIdentity.CognitoUserAttribute(emailData);
+
+	userPool.signUp(email, password, [ emailAttribute ], null, (err, data) =>{
+		if(err){
+			return console.log(err);
+		}
+		res.send(data.user);
+	})
 });
 
 // user Login and Returning token
